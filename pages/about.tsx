c0671dbd0d3875d875a655b7996e6187f508f119
Lakmina Egodawattha <lakmina@bistecglobal.com>
2024-03-18 18:23:34 +0530

    added

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,36 +1,27 @@
 import React, { useState, useEffect } from 'react';
-
 const About = () => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const controller = new AbortController()
-        const signal = controller.signal
-
-        fetch('https://939997652e824928b7aad296d43a6d16.api.mockbin.io/',{signal})
-            .then(response => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://939997652e824928b7aad296d43a6d16.api.mockbin.io/');
+                
                 if (!response.ok) {
                     throw new Error('Failed to fetch data');
                 }
-                return response.json();
-            })
-            .then(data => {
-                if(!signal.aborted){
+                const data = await response.json();
                     setData(data);
                     setLoading(false);
-                }
-            })
-            .catch(error => {
-                if(error.name === "AbortError"){
-                console.log("canalled...!")
-                }
-            });
-
-        return () => {
-           controller.abort()
+      
+            } catch (error) {
+                console.error(error)
+            }
         };
+        fetchData();
     }, []);
+    
 
     return (
         <div>
@@ -51,6 +42,68 @@ const About = () => {
 };
 
 export default About;
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// const About = () => {
+//     const [data, setData] = useState([]);
+//     const [loading, setLoading] = useState(true);
+
+//     useEffect(() => {
+//         const controller = new AbortController();
+//             const signal = controller.signal;
+//         const fetchData = async () => {
+            
+    
+//             try {
+//                 const response = await fetch('https://939997652e824928b7aad296d43a6d16.api.mockbin.io/', { signal });
+                
+//                 if (!response.ok) {
+//                     throw new Error('Failed to fetch data');
+//                 }
+    
+//                 const data = await response.json();
+    
+//                 if (!signal.aborted) {
+//                     setData(data);
+//                     setLoading(false);
+//                 }
+//             } catch (error: any) {
+//                 if (error.name === "AbortError") {
+//                     console.log("Cancelled...!");
+//                 }
+//             }
+//         };
+    
+//         fetchData();
+    
+//         return () => {
+//             controller.abort();
+//         };
+//     }, []);
+    
+
+//     return (
+//         <div>
+//             <h1>About Page</h1>
+//             {loading ? (
+//                 <div>Loading About Page...</div> 
+//             ) : (
+//                 <div>
+//                     {data && (
+//                         <div>
+//                             <pre>{JSON.stringify(data, null, 2)}</pre>
+//                         </div>
+//                     )}
+//                 </div>
+//             )}
+//         </div>
+//     );
+// };
+
+// export default About;
 
 // import React from 'react';
 // import { useQuery } from 'react-query';
--- pages/home.tsx
+++ pages/home.tsx
@@ -4,33 +4,27 @@ const Home = () => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const controller = new AbortController()
-        const signal = controller.signal
 
-        fetch('https://89d7afb7090e4521997a00b34ec7743e.api.mockbin.io/',{signal})
-            .then(response => {
+    useEffect(() => {
+        const fetchData = async () => {
+           
+            try {
+                const response = await fetch('https://89d7afb7090e4521997a00b34ec7743e.api.mockbin.io/');
+    
                 if (!response.ok) {
                     throw new Error('Failed to fetch data');
                 }
-                return response.json();
-            })
-            .then(data => {
-                if(!signal.aborted){
+    
+                const data = await response.json();
                     setData(data);
-                    setLoading(false);
-                }
-            })
-            .catch(error => {
-                if(error.name === "AbortError"){
-                console.log("canalled...!")
-                }
-            });
-
-        return () => {
-           controller.abort()
+            } catch (error: any) {
+                console.error(error)
+            }
         };
+    
+        fetchData();
     }, []);
+    
 
     return ( 
         <div>
